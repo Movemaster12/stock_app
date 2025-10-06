c6fb47fe1ffeed96feee030ce1319b2b98286906
Movemaster12 <djiose45@gmail.com>
2025-10-06 20:18:49 +0100

    Finished line chart

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import yfinance as yf
+# import yfinance as yf
 import altair as alt
 import plotly.graph_objects as go
-from stock_utils import fetch_stock_info, stock_name, fetch_annual_finac, fetch_quarterly_finac, fetch_weekly_price_history
+from stock_utils import fetch_stock_info, stock_name, fetch_annual_finac, fetch_quarterly_finac, fetch_weekly_price_history, calculate_price_change, fetch_daily_price, fetch_weekly_price, fetch_annual_price
 
 st.title('Stock Dashboard')
 my_symbol = st.text_input('Enter a stock ticker', '^GSPC', key='dash') # S&P 500 is default
@@ -13,22 +13,87 @@ if my_symbol and my_symbol.strip():
     try:
         information = fetch_stock_info(my_symbol)
         stock_name(information,my_symbol)
-        st.subheader(f'{information['regularMarketPrice']} {information['currency']}')
+        changes = calculate_price_change(information)
+        # st.write(information)
+        if changes['change_colour'] == 'r':
+            colour = 'red'
+            st.subheader(f'{information['regularMarketPrice']} {information['currency']} :red[{changes['change_num']:.2f} ({changes['change_percent']})]')
+        elif changes['change_colour'] == 'g':
+            colour = 'green'
+            st.subheader(f'{information['regularMarketPrice']} {information['currency']} :green[+{changes['change_num']:.2f} (+{changes['change_percent']})]')
+        else:
+            colour = 'white'
+            st.subheader(f'{information['regularMarketPrice']} {information['currency']} :white[{changes['change_num']:.2f} ({changes['change_percent']})]')
 
         if information.get("marketCap") or information.get("sector")!= None:
-            st.subheader(f'Market Cap: ${information["marketCap"]:,.0f}')
-            st.subheader(f'Sector: {information.get("sector")}')
+            with st.expander('Show more'):        
+                st.subheader(f'Market Cap: ${information["marketCap"]:,.0f}')
+                st.subheader(f'Sector: {information.get("sector")}')
+                st.write(f'{information.get("longBusinessSummary")}')
+
+        # Daily
+        daily_history = fetch_daily_price(my_symbol)
+        daily_history = daily_history.rename_axis('Data').reset_index()
+        daily_line_chart = go.Figure(data=[go.Scatter(x=daily_history['Data'],
+                                                y=daily_history['Open'],
+                                                mode='lines',
+                                                line=dict(
+                                                    color= colour
+                                                ))])
+
+        # Weekly
+        weekly_history = fetch_weekly_price(my_symbol)
+        #st.write(weekly_history)
+        weekly_history = weekly_history.rename_axis('Data').reset_index()
+        weekly_line_chart = go.Figure(data=[go.Scatter(x=weekly_history['Data'],
+                                                       y=weekly_history['Open'],
+                                                       mode='lines',
+                                                       line=dict(
+                                                           color=colour
+                                                       ))])
+        weekly_line_chart.update_xaxes(
+            rangebreaks=[
+                dict(bounds=[17, 9.5], pattern="hour"),
+                dict(bounds=["sat", "mon"])
+
+            ]
+        )
+
+        # Annual
+        annual_history = fetch_annual_price(my_symbol)
+        annual_history = annual_history.rename_axis('Data').reset_index()
+        annual_line_chart=go.Figure(data=[go.Scatter(x=annual_history ['Data'],
+                                                       y=annual_history['Open'],
+                                                       mode='lines',
+                                                       line=dict(
+                                                           color=colour
+                                                       ))])
+        annual_line_chart.update_xaxes(
+            rangebreaks=[
+                dict(bounds=["sat","mon"])
+            ]
+        )
+
+        # Selection over time periods
+        # uodate line colour to represent trend of growth for each period
+        history_selection = st.segmented_control(label='History',options=['1D', '1W', '1Y'], default='1D')
+        if history_selection == '1D':
+            st.plotly_chart(daily_line_chart, use_container_width=True)
+        elif history_selection == '1W':
+            st.plotly_chart(weekly_line_chart, use_container_width=True)
+        elif history_selection == '1Y':
+            st.plotly_chart(annual_line_chart, use_container_width=True)
 
         price_history = fetch_weekly_price_history(my_symbol)
-
-        st.header('Chart')
         price_history = price_history.rename_axis('Data').reset_index()
+        st.header('Weekly Changes')
         candle_stick_chart = go.Figure(data=[go.Candlestick(x=price_history['Data'],
                                        open=price_history['Open'],
                                        low=price_history['Low'],
                                        high=price_history['High'],
                                        close=price_history['Close'])])
 
+        
         st.plotly_chart(candle_stick_chart, use_container_width=True)
 
         if information.get("marketCap") or information.get("sector")!= None:
@@ -41,23 +106,40 @@ if my_symbol and my_symbol.strip():
             if selection == 'Quarterly':
                 quarterly_financials = quarterly_financials.rename_axis('Quarter').reset_index()
                 quarterly_financials['Quarter'] = quarterly_financials['Quarter'].astype(str)
-                revenue_chart = alt.Chart(quarterly_financials).mark_bar(color='red').encode(
-                x='Quarter:O', y='Total Revenue')
-                st.altair_chart(revenue_chart, use_container_width=True)
+                quart_chart_data = quarterly_financials.melt(
+                    id_vars=['Quarter'], value_vars=['Total Revenue', 'Net Income'],
+                    var_name='Metric', value_name='Amount'
+                )
+                combined_quart_chart = alt.Chart(quart_chart_data).mark_bar().encode(
+                    x=alt.X('Quarter:O', title='Quarter'), y=alt.Y('Amount', title=f'Amount {information['currency']}'),
+                    color=alt.Color('Metric:N', scale=alt.Scale(domain=['Total Revenue', 'Net Income'], range=['red','orange'])),
+                    xOffset='Metric:N',
+                    tooltip=[
+                        alt.Tooltip('Quarter:O', title='Quarter'),
+                        alt.Tooltip('Metric:N', title='Metric'),
+                        alt.Tooltip('Amount:Q', title='Amount', format=',')
+                    ]
+                    ).properties(title='Revenue vs Net Income')
+                st.altair_chart(combined_quart_chart, use_container_width=True)
 
-                net_income_chart = alt.Chart(quarterly_financials).mark_bar(color='orange').encode(
-                x='Quarter:O', y='Net Income')
-                st.altair_chart(net_income_chart, use_container_width=True)
+                
             elif selection == 'Annual':
                 annual_financials = annual_financials.rename_axis('Year').reset_index()
                 annual_financials['Year'] = annual_financials['Year'].astype(str).transform(lambda year: year.split('-')[0])
-                revenue_chart = alt.Chart(annual_financials).mark_bar(color='red').encode(
-                x='Year:O', y='Total Revenue')
-                st.altair_chart(revenue_chart, use_container_width=True)
-
-                net_income_chart = alt.Chart(annual_financials).mark_bar(color='orange').encode(
-                x='Year:O', y='Net Income')
-                st.altair_chart(net_income_chart, use_container_width=True)
+                annual_chart_data = annual_financials.melt(
+                    id_vars=['Year'], value_vars=['Total Revenue', 'Net Income'],
+                    var_name='Metric', value_name='Amount'
+                )
+                combined_annual_chart = alt.Chart(annual_chart_data).mark_bar().encode(
+                    x=alt.X('Year:O', title='Year'), y=alt.Y('Amount', title=f'Amount {information['currency']}'),
+                    color=alt.Color('Metric:N', scale=alt.Scale(domain=['Total Revenue', 'Net Income'], range=['red','orange'])),
+                    xOffset='Metric:N',
+                    tooltip=[
+                        alt.Tooltip('Year:O', title='Quarter'),
+                        alt.Tooltip('Metric:N', title='Metric'),
+                        alt.Tooltip('Amount:Q', title='Amount', format=',')]
+                    ).properties(title='Revenue vs Net Income')
+                st.altair_chart(combined_annual_chart, use_container_width=True)
 
     except Exception as e:
         st.error(f' Error occured: {e}')
--- stock_utils.py
+++ stock_utils.py
@@ -25,5 +25,43 @@ def fetch_weekly_price_history(symbol):
     stock = yf.Ticker(symbol)
     return stock.history(period='1y', interval='1wk')
 
+@st.cache_data(ttl=60)
+def fetch_daily_price(symbol):
+    stock = yf.Ticker(symbol)
+    return stock.history(period='1d', interval='1m')
+
+@st.cache_data(ttl=600)
+def fetch_weekly_price(symbol):
+    stock = yf.Ticker(symbol)
+    return stock.history(period='7d', interval='30m')
+
+@st.cache_data(ttl=3600)
+def fetch_annual_price(symbol):
+    stock = yf.Ticker(symbol)
+    return stock.history(period='1y', interval='1d')
+
+@st.cache_data
 def stock_name(info, symbol):
     st.header(f'{info["longName"]} ({symbol})')
+
+@st.cache_data(ttl=60)
+def calculate_price_change(info):
+    current_price = info.get('regularMarketPrice')
+    previous_close = info.get('regularMarketPreviousClose')
+
+    change_num = (current_price - previous_close)
+    change_percent = f'{(change_num / previous_close )*100:.2f}%'
+    if change_num > 0:
+        change_colour = 'g'
+    elif change_num < 0:
+        change_colour = 'r'
+    else:
+        change_colour = 'w'
+
+    change_dict = {
+        'change_num': change_num,
+        'change_percent': change_percent,
+        'change_colour' : change_colour
+    }
+
+    return change_dict
